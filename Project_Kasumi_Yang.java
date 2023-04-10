1fc7acbcd270e374e329591c2127c8fb32afc0f7
Kasumi Yang <kyang017@email.cpcc.edu>
2023-04-10 01:14:53 -0400

    txt file
    File that contains policy information
    Modified to read input from a file
    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_Kasumi_Yang.java
+++ Project_Kasumi_Yang.java
@@ -1,76 +1,75 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class Project_Kasumi_Yang
 {
-   public static void main(String[] args)
-   {
-      Scanner keyboard = new Scanner(System.in); //Create scanner object
-      
-      int testNum; //To hold policy number
-      String testProName; //To hold provider name
-      String testFName; //Policyholder first name
-      String testLName; //Policyholder last name
-      int testAge; //Hold Policyholder age
-      String testStatus; //Policyholder smoking status
-      double testWeight; //Policyholder's weight
-      double testHeight; //Policyholder's height
-      
-      //Get policy number from user
-      System.out.print("Please enter the Policy Number: ");
-      testNum = keyboard.nextInt();
-      
-      keyboard.nextLine();
-      
-      //Get provider name from user
-      System.out.print("Please enter the Provider Name: ");
-      testProName = keyboard.nextLine();
-      
-      //Get policyholder's first name
-      System.out.print("Please enter the Policyholder's First Name: ");
-      testFName = keyboard.nextLine();
-      
-      //Get policyholder's last name
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      testLName = keyboard.nextLine();
-      
-      //Get policyholder's age
-      System.out.print("Please enter the Policyholder's Age: ");
-      testAge = keyboard.nextInt();
-      
-      keyboard.nextLine();
-                  
-      //Get policyholder's smoking status
-      System.out.print("Please enter the Policyholder's Smoking"
-                           + " Status (smoker/non-smoker): ");
-      testStatus = keyboard.nextLine();
-      
-      //Get policyholder's height
-      System.out.print("Please enter the Policyholder's Height (in inches): ");
-      testHeight = keyboard.nextDouble();
-      
-      //Get policyholder's weight
-      System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-      testWeight = keyboard.nextDouble();
-      
-      /**
-         Create an instance of the Policy class,
-         passing the data entered as arguments
-         to the constructor
-      */     
-      Policy p = new Policy(testNum, testProName, testFName, testLName,
-                           testAge, testStatus, testHeight, testWeight);
-                                 
-      //Get data and display it
-      System.out.println();
-      System.out.print("\nPolicy Number: " + p.getPolicyNumber());
-      System.out.print("\nProvider Name: " + p.getProvidersName());
-      System.out.print("\nPolicyholder's First Name: " + p.getFirstName());
-      System.out.print("\nPolicyholder's Last Name: " + p.getLastName());
-      System.out.print("\nPolicyholder's Age: " + p.getPolicyAge());
-      System.out.print("\nPolicyholder's Smoking Status: " + p.getSmokingStatus());
-      System.out.print("\nPolicyholder's Height: " + p.getHeight() + " inches");
-      System.out.print("\nPolicyholder's Weight: " + p.getWeight() + " pounds");
-      System.out.printf("\nPolicyholder's BMI: %,.2f", p.getPolicyHolderBMI());
-      System.out.printf("\nPolicy Price: $%,.2f", p.getInsurancePrice());
-   }
+    public static void main(String[] args) throws IOException
+    {
+        
+        File file = new File("PolicyInformation.txt");
+        Scanner inputFile = new Scanner(file);
+        
+        //Declare variables
+        int testNum;
+        String testProName;
+        String testFName;
+        String testLName;
+        int testAge;
+        String testStatus;
+        double testHeight;
+        double testWeight;
+        double bmi;
+        double policyPrice = 600.00;
+        
+        int smokerTotal = 0;
+        int nonSmokerTotal = 0;
+        
+        //while loop
+        while(inputFile.hasNext())
+        {
+            testNum = inputFile.nextInt();
+            inputFile.nextLine();
+            testProName = inputFile.nextLine();
+            testFName = inputFile.nextLine();
+            testLName = inputFile.nextLine();
+            testAge = inputFile.nextInt();
+            inputFile.nextLine();
+            testStatus = inputFile.nextLine();
+            testHeight = inputFile.nextDouble();
+            testWeight = inputFile.nextDouble();
+            bmi = getPolicyHolderBMI(testHeight, testWeight);
+            
+            //if else statement for finding how many are non-smokers or smokers
+            if (testStatus.equalsIgnoreCase("smoker"))
+            {
+                smokerTotal++;
+            }
+            else
+            {
+                nonSmokerTotal++;
+            }
+            
+            //display information
+            System.out.println("\nPolicy Number: " + testNum);
+            System.out.println("Provider Name: " + testProName);
+            System.out.println("Policyholder's First Name: " + testFName);
+            System.out.println("Policyholder's Last Name: " + testLName);
+            System.out.println("Policyholder's Age: " + testAge);
+            System.out.println("Policyholder's Smoking Status (smoker/non-smoker): " + testStatus);
+            System.out.println("Policyholder's Height: " + testHeight + " inches");
+            System.out.println("Policyholder's Weight: " + testWeight + " pounds");
+            System.out.printf("Policyholder's BMI: %.2f%n", bmi);
+            System.out.printf("Policy Price: $%.2f%n", policyPrice); 
+        } //end of while loop
+
+        inputFile.close();
+
+        System.out.println("\nThe number of policies with a smoker is: " + smokerTotal);
+        System.out.println("The number of policies with a non-smoker is: " + nonSmokerTotal);
+    }
+    
+    public static double getPolicyHolderBMI(double height, double weight)
+    {
+        return (weight * 703) / Math.pow(height, 2);
+    }
 }
